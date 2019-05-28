449fbb4012e08b90c292bad459c7a30bff3bc0a1
Alex Taylor <alex.taylor@talktalkplc.com>
2019-05-28 15:36:24 +0100

    add basic site routing

---
+++ src/pages/router.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+function BasicRouter() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Route exact path="/" component={Home} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/contact" component={Contact} />
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function Projects() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Contact() {
+  return (
+    <div>
+      <h2>Contact</h2>
+    </div>
+  );
+}
+
+export default BasicRouter;
