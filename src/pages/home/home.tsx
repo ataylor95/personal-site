c23b7f7d457be84ad7b909cd4dd439d6f25bc637
Alex Taylor <alex.taylor@talktalkplc.com>
2019-06-24 17:00:56 +0100

    create basic layout for cards on the home page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,28 +1,19 @@
 import React from "react";
 
 import Page from '../../components/page/page';
+import ProjectCard from '../../components/project-card/project-card';
 
 const Home: React.FC = () => {
     return (
       <Page>
-        <div>
-          <h2>Home</h2>
-          <p>Alex Taylor</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In hendrerit gravida rutrum quisque non. Facilisis sed odio morbi quis commodo odio. Eget nullam non nisi est sit amet facilisis. Id aliquet risus feugiat in ante metus dictum. Nunc sed id semper risus in hendrerit gravida rutrum. Cras tincidunt lobortis feugiat vivamus at augue eget. Aliquam malesuada bibendum arcu vitae elementum curabitur vitae nunc. Ridiculus mus mauris vitae ultricies leo integer malesuada nunc. A pellentesque sit amet porttitor eget. Eleifend mi in nulla posuere sollicitudin.</p>
-
-          <p>Ac placerat vestibulum lectus mauris ultrices eros in cursus. Auctor eu augue ut lectus arcu bibendum at varius vel. Enim blandit volutpat maecenas volutpat blandit aliquam etiam. Pharetra massa massa ultricies mi. Mauris pellentesque pulvinar pellentesque habitant morbi. Feugiat nibh sed pulvinar proin gravida hendrerit. Amet facilisis magna etiam tempor orci eu lobortis elementum nibh. Diam phasellus vestibulum lorem sed risus ultricies tristique. Gravida rutrum quisque non tellus orci ac auctor. Id ornare arcu odio ut sem. Adipiscing diam donec adipiscing tristique risus nec feugiat in fermentum. Duis tristique sollicitudin nibh sit amet commodo nulla facilisi nullam. Amet commodo nulla facilisi nullam. Egestas quis ipsum suspendisse ultrices gravida dictum.</p>
-
-          <p>Euismod in pellentesque massa placerat. Mi bibendum neque egestas congue quisque. Dapibus ultrices in iaculis nunc sed augue. Scelerisque in dictum non consectetur a erat nam. Dictumst quisque sagittis purus sit amet volutpat. Hac habitasse platea dictumst quisque sagittis purus. Vulputate enim nulla aliquet porttitor lacus. At lectus urna duis convallis convallis tellus id interdum. Justo eget magna fermentum iaculis. Risus nullam eget felis eget.</p>
-
-          <p>Tortor dignissim convallis aenean et tortor. Leo urna molestie at elementum eu facilisis sed odio morbi. In egestas erat imperdiet sed euismod nisi porta. Tincidunt augue interdum velit euismod in pellentesque massa placerat duis. Neque vitae tempus quam pellentesque nec nam aliquam sem. Arcu dictum varius duis at consectetur. Ut diam quam nulla porttitor massa id neque. Tempor id eu nisl nunc mi ipsum faucibus vitae. Adipiscing diam donec adipiscing tristique. Ullamcorper sit amet risus nullam eget felis. In egestas erat imperdiet sed euismod nisi porta. Et netus et malesuada fames ac. Nibh sit amet commodo nulla facilisi nullam vehicula.</p>
-
-          <p>Libero nunc consequat interdum varius sit amet mattis vulputate enim. Adipiscing vitae proin sagittis nisl. Magnis dis parturient montes nascetur ridiculus mus mauris vitae. Amet justo donec enim diam. Consequat id porta nibh venenatis cras sed. Ac feugiat sed lectus vestibulum mattis. Elementum sagittis vitae et leo duis ut diam. Pellentesque nec nam aliquam sem et tortor consequat. Cras ornare arcu dui vivamus arcu felis bibendum. Netus et malesuada fames ac turpis. Consequat mauris nunc congue nisi vitae. Ut tristique et egestas quis.</p>
-
-          <p>Massa sed elementum tempus egestas sed sed. Nulla posuere sollicitudin aliquam ultrices sagittis orci a. In fermentum et sollicitudin ac orci. Lorem dolor sed viverra ipsum nunc aliquet bibendum enim. Vel turpis nunc eget lorem dolor sed viverra ipsum. Donec enim diam vulputate ut pharetra sit amet. Mattis ullamcorper velit sed ullamcorper morbi tincidunt. Elit ullamcorper dignissim cras tincidunt. Vitae congue mauris rhoncus aenean vel. Arcu risus quis varius quam quisque id diam vel quam. Suspendisse interdum consectetur libero id faucibus nisl tincidunt eget. Quam quisque id diam vel quam elementum.</p>
-
-          <p>Quam nulla porttitor massa id neque aliquam vestibulum morbi. Nulla aliquet porttitor lacus luctus accumsan tortor posuere. Cursus euismod quis viverra nibh cras pulvinar mattis nunc sed. Elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus. Maecenas sed enim ut sem. Non enim praesent elementum facilisis leo vel. Ullamcorper sit amet risus nullam eget felis eget nunc lobortis. Malesuada fames ac turpis egestas sed tempus urna. Tellus orci ac auctor augue mauris augue neque gravida. Adipiscing diam donec adipiscing tristique risus nec feugiat in fermentum. Aliquam purus sit amet luctus venenatis lectus. Parturient montes nascetur ridiculus mus mauris vitae. Velit laoreet id donec ultrices tincidunt. Sit amet volutpat consequat mauris. Amet nisl suscipit adipiscing bibendum est ultricies integer quis.</p>
-
-          <p>Amet dictum sit amet justo donec. Risus quis varius quam quisque. Risus at ultrices mi tempus. Dui nunc mattis enim ut tellus elementum sagittis. Sagittis eu volutpat odio facilisis mauris sit amet massa. Amet luctus venenatis lectus magna fringilla. Mattis enim ut tellus elementum sagittis vitae et leo. Velit egestas dui id ornare arcu odio ut. Turpis massa tincidunt dui ut ornare lectus sit amet. Aliquam ultrices sagittis orci a.</p>
+        <div className="home-page">
+          <ProjectCard name="Example" description="An example description" link="/"></ProjectCard>
+          <ProjectCard name="Example" description="An example description" link="/"></ProjectCard>
+          <ProjectCard name="Example" description="An example description" link="/"></ProjectCard>
+          <ProjectCard name="Example" description="An example description" link="/"></ProjectCard>
+          <ProjectCard name="Example" description="An example description" link="/"></ProjectCard>
+          <ProjectCard name="Example" description="An example description" link="/"></ProjectCard>
+          <ProjectCard name="Example" description="An example description" link="/"></ProjectCard>
         </div>
       </Page>
     );
