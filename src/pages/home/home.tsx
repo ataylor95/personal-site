584aff7550ab7186c2c720807d1f3a4e78c7310e
Alex Taylor <alex.taylor@talktalkplc.com>
2019-06-25 14:33:09 +0100

    move t5he list of porjects into a json object to make it easier to edit them rather than hardcode

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -3,21 +3,21 @@ import React from "react";
 import Page from '../../components/page/page';
 import ProjectCard from '../../components/project-card/project-card';
 
+import projects from '../../data/projects.json';
+
 const Home: React.FC = () => {
-    return (
-      <Page>
-        <h1>Projects</h1>
-        <div className="home-page__container">
-          <ProjectCard name="Example" description="An example description" link="/"></ProjectCard>
-          <ProjectCard name="Example" description="An example description" link="/"></ProjectCard>
-          <ProjectCard name="Example" description="An example description" link="/"></ProjectCard>
-          <ProjectCard name="Example" description="An example description" link="/"></ProjectCard>
-          <ProjectCard name="Example" description="An example description" link="/"></ProjectCard>
-          <ProjectCard name="Example" description="An example description" link="/"></ProjectCard>
-          <ProjectCard name="Example" description="An example description" link="/"></ProjectCard>
-        </div>
-      </Page>
-    );
+  let projectCards = projects.projects.map(project => {
+    return <ProjectCard name={project.name} description={project.description} link={project.link}></ProjectCard>
+  });
+
+  return (
+    <Page>
+      <h1>Projects</h1>
+      <div className="home-page__container">
+        {projectCards}
+      </div>
+    </Page>
+  );
   }
 
 export default Home;
