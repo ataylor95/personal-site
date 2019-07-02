7578a027a2ca0aaa3244726a02c32acc4906d45a
Alex Taylor <alex.taylor@talktalkplc.com>
2019-07-02 09:27:07 +0100

    nav styling stuff

--- src/components/page/page.tsx
+++ src/components/page/page.tsx
@@ -10,7 +10,7 @@ const Page: React.FC<pageProps> = (props) => {
     <div className="page">
         <div className="page__header__logo">Logo</div>
         <div className="page__header__content">
-            <h1>amltaylor</h1>
+            <h1>Alex (Michael Levi) Taylor</h1>
             <h2>Full Stack Developer</h2>
             <h3>London</h3>
             <a href="mailto: abc@example.com">Email me</a>
