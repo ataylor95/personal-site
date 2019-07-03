debefe0528dff9c0394d988cb19181df98fe0c5f
Alex Taylor <alex.taylor@talktalkplc.com>
2019-07-03 21:46:14 +0100

    Add tech names below the logos

--- src/components/tech-icon/tech-icon.tsx
+++ src/components/tech-icon/tech-icon.tsx
@@ -6,15 +6,19 @@ interface techIconProps {
 
 const TechIcon: React.FC<techIconProps> = ({name}) => {
     let [imgsrc, setImgsrc] = useState('');
-    import(`../../data/images/logos/${name}-icon.svg`).then((logo) => {
+    const imgName = name.toLowerCase().replace(' ', '');
+    import(`../../data/images/logos/${imgName}-icon.svg`).then((logo) => {
         setImgsrc(logo.default);
     });
 
-    const addWhiteBg = (name.includes('stencil') ? "add-white-bg" : "")
+    // Stencil logo is black so need to add a white background.
+    // In theory if there are more logos like this, we could make an array of them to check against
+    const addWhiteBg = (name.includes('Stencil') ? "add-white-bg" : "")
 
     return (
         <div className="tech-icon">
             <img className={`tech-icon__icon ` + addWhiteBg}  src={imgsrc} alt={name} title={name}></img>
+            <p>{name}</p>
         </div>
     );
 }
